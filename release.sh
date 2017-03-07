4816c7affdbe87146985737f1dde46a2de78e050
David Keller <david.keller@enyx.fr>
2017-03-07 10:51:20 +0100

    Enhance release.sh.
    Prompt with a default value to 'y'.
    Tag and push tag on origin repository.
    
    Signed-off-by: David Keller <david.keller@enyx.fr>

--- release.sh
+++ release.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 
+set -e
+
 cd $(dirname $0)
 
 version=$(python -c 'import yassh; print yassh.__version__')
 
-rm dist/*
+rm -rf dist/*
 python setup.py sdist bdist_wheel
 
 while true; do
-    read -p "Do you wish to upload ${version} to pypi.python.org?" yn
+    read -p "Do you wish to upload ${version} to pypi.python.org [Yn] ?" yn
+    yn=${yn:-y}
     case $yn in
-        [Yy]*)
+        [Yy])
             twine upload dist/*
+            git tag v${version}
+            git push origin v${version}
             break
             ;;
-        [Nn]*)
+        [Nn])
             break
             ;;
         *)
